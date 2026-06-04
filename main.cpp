76a499b9ea2a4d465efcebb6aff82d0fc75db9f1
Bhavishya-07 <bhavishyaaggarwal890@gmail.com>
2026-06-04 20:49:00 +0530

    Day 3: Mastered conditional logic and top-down edge cases

--- main.cpp
+++ main.cpp
@@ -1,20 +1,29 @@
 #include <iostream>
 
 int main() {
-    char teamTier;
-    int characterCount;
-    float avgAttack;
+    int playerHP;
 
-    std::cout << "Enter Team Tier Letter: " << std::endl;
-    std::cin >> teamTier;
+    std::cout  << "Enter player's HP: ";
+    std::cin >> playerHP;
 
-    std::cout << "Enter Number of Active Characters: " << std::endl;
-    std::cin >> characterCount;
+    if (playerHP <= 0){
+        std::cout << "Game Over!" << std::endl;
+    }
+    
+    else if (playerHP == 100){
+        std::cout << "Full HP! Ready to blast!" << std::endl;
+    }
+    else if (playerHP >= 80){
+        std::cout << "Health is stable!" << std::endl;
+    }
 
-    std::cout << "Enter Average Character Attack: " << std::endl;
-    std::cin >> avgAttack;
+    else if (playerHP >= 40){
+        std::cout << "Decent HP, but be carefull." << std::endl;
+    }
 
-    std::cout << "Squad" << teamTier << "loaded." << "Total Squad Potential: " << characterCount * avgAttack << std::endl;
-
+    else{
+        std::cout << "Warning: Low Health! Heal up!" << std::endl;
+    }
+    
     return 0;
 }
