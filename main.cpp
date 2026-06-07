306eeca4c2aa1b7589c5901599bfe19051e4639a
Bhavishya-07 <bhavishyaaggarwal890@gmail.com>
2026-06-07 16:25:44 +0530

    Day 6: Mastered for and while loops

--- main.cpp
+++ main.cpp
@@ -1,25 +1,14 @@
 #include <iostream>
 
 int main() {
-    int menuChoice;
 
-    std::cout << "1. Thor\n2. Iron Man\n3. Storm\nSelect your hero number: ";
-    std::cin >> menuChoice;
-
-    switch (menuChoice) {
-        case 1: 
-            std::cout << "Thor deployed! Bring the thunder!" << std::endl;
-            break;
-        case 2: 
-            std::cout << "Iron Man deployed! Repulsors charged!" << std::endl;
-            break;
-        case 3:
-            std::cout << "Storm deployed! Power of the elements!" << std::endl;
-
-        default:
-            std::cout << "Invalid selection! Team empty." << std::endl;
-        
+    for (int a = 2; a <= 8; a += 3) {
+        for (int b = 1; b <= 7; b+=3) {
+            if (a > b && (a+b) % 3 == 0){
+                std::cout << "(" << a << "," << b << ")" << std::endl;
+            }
+        }
+    
     }
-
     return 0;
 }
