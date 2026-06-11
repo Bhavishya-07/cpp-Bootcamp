40ed084d27e6a09fcbad542986a17a56970ccd2c
Bhavishya-07 <bhavishyaaggarwal890@gmail.com>
2026-06-11 19:10:21 +0530

    Day 9: Mastered 2D arrays, matrix index mapping, and diagonal identity operations

--- main.cpp
+++ main.cpp
@@ -1,18 +1,41 @@
 #include <iostream>
 
-int main() {
-    
-    int scores[] = {45, 89, 72, 12, 95, 90};
-    int super = 0;
-    for (int i = 0; i <= 7; i++) {
-    
-        if (scores[i] >= super) {
-            super = scores[i];
+int main(){
+
+    int rowSums[] = {0, 0, 0};
+    int a = 0;
+    int j = 0; 
+    int grid[][3] {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+
+    for (int i = 0; i < 3; i++){
+        for (j = 0; j < 3; j++){
+            a += grid[i][j];
+        }    
+        rowSums[i] = a;
+        if (a % 2 == 0){
+            for (j = 0; j < 3; j++){
+                grid[i][j] = 0;
+                std::cout << grid[i][j] << " ";
+            }
+            
         }
         else {
+            for (j = 0; j < 3; j++){
+                grid[i][j] = 1;
+                std::cout << grid[i][j] << " ";
+            }
         }
+        std::cout << std::endl;
+        a = 0;
         
     }
-    std::cout << "The player who scored " << super << " wins!" << std::endl;
     
+            
+        
+    
+    return 0;
 }
