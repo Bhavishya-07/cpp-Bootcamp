9202aa651bef9c863173cf23544638e6f668a240
Bhavishya-07 <bhavishyaaggarwal890@gmail.com>
2026-06-25 22:48:00 +0530

    Day 12: Implement manual string manipulation, reference loops, and palindrome detector

--- main.cpp
+++ main.cpp
@@ -1,33 +1,20 @@
 #include <iostream>
-
-void calculateTotalInventory(const int grid[][3][3], int layers){
-    int sum = 0;
-    for (int k = 0; k < layers; k++){
-        for (int i = 0; i < 3; i++){
-            for (int j = 0; j < 3; j++){
-                sum += grid[k][i][j];
-
-            }
-        }
-    } std::cout << "Sum of elements of the cube: " << sum << std::endl;
-}
+#include <string>
+#include <algorithm>
+#include <cctype>
 
 int main(){
-    int grid[][3][3] {
-    {  {5, 5, 5},
-        {5, 5, 5},
-        {5, 5, 5}
-    },
-    { 
-        {10,10,10},
-        {10,10,10},
-        {10,10,10}
-    }
-};
 
-    int layers = 2;
-    calculateTotalInventory(grid, layers);
+    std::string alpha;
+    std::cout << "Enter the message: ";
+    std::getline(std::cin, alpha);
+
+    std::transform(alpha.begin(), alpha.end(), alpha.begin(), ::toupper);
+
+    std::reverse(alpha.begin(), alpha.end());
+
+    std::cout << alpha << "\n";
+
+
+    return 0;
 }
-
-
-
