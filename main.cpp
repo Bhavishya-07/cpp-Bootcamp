d8b54d4840c31c1edf2adf9c5097f5d81e7dbb23
Bhavishya-07 <bhavishyaaggarwal890@gmail.com>
2026-06-29 15:49:14 +0530

    Day 16: Mastery of key-value map systems with advanced pointer indirection lookups and .count verification

--- main.cpp
+++ main.cpp
@@ -2,25 +2,36 @@
 #include <string>
 #include <map>
 
+void CTPchecker(const std::map<std::string, int> &premium_inventory, const std::string* search);
+
 int main() {
-    std::map<std::string, int> gear_inventory;
+    
+    std::map<std::string, int> premium_inventory;
 
-    gear_inventory["CTP of Energy"] = 4;
-    gear_inventory["Mighty CTP"] = 1;
-    gear_inventory["Brilliant CTP"] = 0;
+    premium_inventory["Mighty CTP"] = 1;
+    premium_inventory["Refined CTP"] = 3;
 
-    std::string audit1 = "CTP of Energy";
-    std::string audit2 = "Brilliant CTP";
+    std::string target1 = "Mighty CTP";
+    std::string target2 = "Refined CTP";
 
-    std::string* current_audit;
+    std::string* search;
+    search = &target1;
 
-    current_audit = &audit1;
+    CTPchecker(premium_inventory, search);
 
-    std::cout << *current_audit << " is in the slot " << gear_inventory[*current_audit] << std::endl;
+    std::string seach = "Brilly Rage";
+    search = &seach;
 
-    current_audit = &audit2;
-
-    std::cout << *current_audit << " is in the slot " << gear_inventory[*current_audit] << std::endl;
+    CTPchecker(premium_inventory, search);
 
     return 0;
+}
+
+void CTPchecker(const std::map<std::string, int> &premium_inventory, const std::string* search){
+    if (premium_inventory.count(*search) == 1){
+        std::cout << premium_inventory.at(*search) << " " << *search << " are available.\n";
+    }
+    else{
+        std::cout << "No such CTP available" << std::endl;
+    }
 }
